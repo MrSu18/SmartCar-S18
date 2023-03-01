186803d5fcce7469bd47908582e9f8bd750cc38a
MrSu18 <3051605203@qq.com>
2023-03-01 15:05:35 +0800

    把逐飞旧版本的发送灰度图到上位机函数移植

--- software/Seekfree_TC264_Opensource_Library/code/bluetooth.h
+++ software/Seekfree_TC264_Opensource_Library/code/bluetooth.h
@@ -17,5 +17,6 @@
 #define BLUETOOTH_RATE  (115200)               //蓝牙模块的串口波特率
 
 void UARTInit(void);
+void seekfree_sendimg_03x(uart_index_enum uartn, uint8 *image, uint16 width, uint16 height);//把图像发送给逐飞上位机
 
 #endif /* CODE_BLUETOOTH_H_ */
